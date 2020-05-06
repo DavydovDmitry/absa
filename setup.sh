b3bfd19ab0d140f2cb393c77ed352689ce83b5a1
davydovdmitry <davydofdmitry@gmail.com>
2020-05-06 20:08:59 +0300

    setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# Install requirements
+poetry shell
+poetry install --no-dev
+printf 'y\n\n' | python -c 'import stanfordnlp; stanfordnlp.download("ru")'
+
+# Download embeddings
+wget http://vectors.nlpl.eu/repository/20/185.zip -P ./RusVectores/
+unzip RusVectores/185.zip -d RusVectores/tayga_upos_skipgram_300_2_2019
+rm RusVectores/185.zip
+
+# Download dataset
+mkdir ./datasets/SemEval2016/
+wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1RZUyBrWQ0OwlIsmN0axewKg21koYmgQf' -O ./datasets/SemEval2016/train.xml
+wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1JR3gblfNXQHApmDzY4FCCjv_0wVug7dO' -O ./datasets/SemEval2016/test.xml
